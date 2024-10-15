5a7ce6f18fbd9cc1e6221ad05cdcd3482a56d957
bharaths1803 <bharaths14051803@gmail.com>
2024-10-15 20:59:12 +0530

    Committing blog application for backend

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const {userRouter} = require("./routes/user.js");
+const {blogRouter} = require("./routes/blogs.js");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/blog", blogRouter);
+
+
+
+async function main(){
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () =>{
+    console.log(`Listening on port ${port}`);
+  });
+}
+
+
+
+main();
